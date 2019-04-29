c71cb0a8957e38478ee552bb02c8fb85af30d693
johnjasa <johnjasa@umich.edu>
2019-04-29 15:19:16 -0400

    Added all cases. They 01 well locally

---
+++ waspy/CRM/baseline/run_aerostruct.py
@@ -0,0 +1,19 @@
+from __future__ import division, print_function
+
+from waspy.master_functions import setup_and_run_problem
+
+
+# Set these individual settings for each problem. Later functions
+# will parse these and set up the model correctly.
+case_settings = {
+    'planform' : 'CRM',
+    'with_viscous' : False,
+    'with_wave' : False,
+    'compressibility' : False,
+    'struct_weight_relief' : False,
+    'distributed_fuel_weight' : False,
+    'engine_mass' : False,
+    'engine_thrust' : False,
+    }
+
+setup_and_run_problem(case_settings)
